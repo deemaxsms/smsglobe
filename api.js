2651d6c9cb592b06c5fa97c3cea6df9b8a3937c5
Keah Logistics <keahlogisticsq@gmail.com>
2026-08-01 08:13:18 +0100

    fxuere

--- api.js
+++ api.js
@@ -573,6 +573,12 @@ app.all('/api/:action', async (req, res) => {
      case 'rdp-request-complete': // This matches the fetch URL in your HTML file
     if (req.method === 'POST') return handleCompleteRDPOrder(req, res);
     break; 
+    case 'services':
+        if (req.method === 'GET') return handleGetServices(req, res);
+        break;
+    case 'countries':
+        if (req.method === 'GET') return handleGetCountries(req, res);
+        break;
     case 'get-numbers': return handleGetNumbers(req, res);
     case 'get-countries': return handleGetCountries(req, res);
      case 'get-stock':  return handleGetStock(req, res);
